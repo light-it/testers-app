683dc52d020a88cbd07be8aab0a9916cc85fcb56
Ruslan K <rkotsuruba@gmail.com>
2016-07-22 15:34:13 +0300

    add flass message on sign up

--- app/controllers/my_registrations_controller.rb
+++ app/controllers/my_registrations_controller.rb
@@ -4,6 +4,7 @@ class MyRegistrationsController < Devise::RegistrationsController
     super
     if @user.persisted?
       UserMailer.new_registration(@user).deliver!
+      flash[:notice] = "You have successfully signed up on the website. Login and password were sent to your mail."
     end
   end
 
