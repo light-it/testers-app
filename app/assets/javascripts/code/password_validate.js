25bb2514edcf5177b72a48d5dee0bb1320824541
floganz <alexander.0072@gmail.com>
2016-07-26 08:54:28 +0300

    one of the listed file types doesn't allowed, bug 14

--- app/assets/javascripts/code/password_validate.js
+++ app/assets/javascripts/code/password_validate.js
@@ -8,8 +8,25 @@ $(document).on("turbolinks:load",function(){
     errorMessage : 'Password does not match',
     errorMessageKey: 'badPasswordConfirmation'
   });
+  // file validator with bug
+  $.formUtils.addValidator({
+        name : 'file_check',
+        validatorFunction : function(value, $el, config, language, $form) {
+          var allowedFiles = [".jpg", ".jpeg", ".png"];
+          var regex = new RegExp("([a-zA-Z0-9\s_\\.\-:])+(" + allowedFiles.join('|') + ")$");
+          return regex.test(value.toLowerCase())
+        },
+        errorMessage : 'Only files of type jpg, png, gif, jpeg is allowed',
+        errorMessageKey: 'wrongFiletype'
+  });
 
   $.validate({
     modules : 'security, file'
+  });
+  // error generation
+  $("#new_user").on('click','input:submit',function(){
+    console.log("1")
+    $("form div:first-child").removeClass("has-success").addClass("has-error");
+    setTimeout(function(){},1000);
   });
 });
