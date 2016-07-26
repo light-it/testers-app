b6a0942bf6bf41fdadc6f612ff28805b8e1527b9
floganz <alexander.0072@gmail.com>
2016-07-26 10:13:02 +0300

    email validation error doesn't display error message at edit form

--- app/assets/javascripts/code/password_validate.js
+++ app/assets/javascripts/code/password_validate.js
@@ -9,7 +9,7 @@ $(document).on("turbolinks:load",function(){
     errorMessageKey: 'badPasswordConfirmation'
   });
 
-  // file validator with bug
+  // file validator with bug (doesnt support all mentioned file types)
   $.formUtils.addValidator({
         name : 'file_check',
         validatorFunction : function(value, $el, config, language, $form) {
@@ -21,6 +21,38 @@ $(document).on("turbolinks:load",function(){
         },
         errorMessage : 'Only files of type jpg, png, gif, jpeg is allowed',
         errorMessageKey: 'wrongFiletype'
+  });
+
+  // email check with bug (no message displayed)
+  $.formUtils.addValidator({
+      name: 'email_check',
+      validatorFunction: function (email) {
+
+        var emailParts = email.toLowerCase().split('@'),
+          localPart = emailParts[0],
+          domain = emailParts[1];
+
+        if (localPart && domain) {
+
+          if( localPart.indexOf('"') === 0 ) {
+            var len = localPart.length;
+            localPart = localPart.replace(/\"/g, '');
+            if( localPart.length !== (len-2) ) {
+              return false; // It was not allowed to have more than two apostrophes
+            }
+          }
+
+          return $.formUtils.validators.validate_domain.validatorFunction(emailParts[1]) &&
+            localPart.indexOf('.') !== 0 &&
+            localPart.substring(localPart.length-1, localPart.length) !== '.' &&
+            localPart.indexOf('..') === -1 &&
+            !(/[^\w\+\.\-\#\-\_\~\!\$\&\'\(\)\*\+\,\;\=\:]/.test(localPart));
+        }
+
+        return false;
+      },
+      errorMessage: '',
+      errorMessageKey: ''
   });
 
   $.validate({
